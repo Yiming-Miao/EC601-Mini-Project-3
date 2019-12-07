6d60cd0117418c6e46b2f4c14552da4986e7964a
Yiming-Miao <55363521+Yiming-Miao@users.noreply.github.com>
2019-12-07 14:58:35 -0500

    Add files via upload

---
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by 缪一鸣 on 2019/12/5.
+//  Copyright © 2019 Yiming Miao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World! This app page was written from Xcode with swift")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
